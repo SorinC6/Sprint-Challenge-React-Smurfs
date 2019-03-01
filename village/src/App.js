ea2c8b4e5c46af49940dc889078b4dd89dc4836f
Sorin Chis <kish.sorin@yahoo.com>
2019-03-01 10:32:53 +0200

    added more smurf on the server, reder smurf on the screen, added error  and loading message

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: [],
+			error: '',
+			loading: false
+		};
+	}
+	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+
+	componentDidMount() {
+		this.startSpinner();
+		axios
+			.get('http://localhost:3333/smurfs')
+			.then((res) => this.setState({ smurfs: res.data }))
+			.catch((err) => this.setState({ error: err.message }))
+			.finally(this.stopSpinner());
+	}
+
+	startSpinner = () => {
+		this.setState({ loading: true });
+	};
+	stopSpinner = () => {
+		this.setState({ loading: false });
+	};
+
+	render() {
+		if (this.state.loading) {
+			return <div style={{ textAlign: 'center', color: 'red' }}>Loading...</div>;
+		}
+
+		if (this.state.error) {
+			return (
+				<div style={{ textAlign: 'center', color: 'red' }}>
+					<div>Argh! This failed rather miserably. {this.state.error.message}</div>
+				</div>
+			);
+		}
+		return (
+			<div className="App">
+				<SmurfForm />
+				<Smurfs smurfs={this.state.smurfs} />
+			</div>
+		);
+	}
 }
 
 export default App;
